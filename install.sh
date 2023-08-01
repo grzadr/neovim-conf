de2c07de178c63be679a746b84febd72cc17fc8a
Adrian Grzemski <adrian.grzemski@gmail.com>
2023-08-01 14:04:26 +0200

    added plugin install

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ set -o pipefail
 
 CONFIG_DIR=$HOME/.config/nvim
 UNDO_DIR=$HOME/.local/share/nvim/undo
+PLUGIN_DIR=$HOME/.local/share/nvim/site/pack/packer/start/packer.vim
 
-mkdir -p ${CONFIG_DIR}
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $PLUGIN_DIR
+
 mkdir -p ${UNDO_DIR}
 
-cp ./init.lua ${CONFIG_DIR}/init.lua
+rm -r ${CONFIG_DIR}
+mkdir -p ${CONFIG_DIR}
+cp -r nvim ${CONFIG_DIR}
 
